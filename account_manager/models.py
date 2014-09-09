3c823195f8a578073ff8df01a51da31184207f38
John Meinken <thebellsofohio@hotmail.com>
2014-09-08 22:42:07 -0400

    delete redundant src files

--- account_manager/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-# from django.contrib import admin
-
-class account_data(models.Model):
-    username = models.CharField(max_length=30)
-    key = models.CharField(max_length=30)
-    value = models.CharField(max_length=30, null=True, blank=True)
-    class Meta:
-        unique_together = ("username", "key")
